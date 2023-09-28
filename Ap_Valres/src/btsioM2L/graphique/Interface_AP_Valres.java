1512df82240d7d3e082e1225792b462f8bcad7ed
Loïs <118986126+TRLois@users.noreply.github.com>
2023-09-28 10:41:44 +0200

    Ajout

---
+++ Ap_Valres/src/btsioM2L/graphique/Interface_AP_Valres.java
@@ -0,0 +1,16 @@
+package btsioM2L.graphique;
+
+import javax.swing.JPanel;
+
+public class Interface_AP_Valres extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Interface_AP_Valres() {
+
+	}
+
+}
